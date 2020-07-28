554b12e4cf1e7df5e782dbf9d0ec0443e7403f7a
joshua killingsworth <kwth@pm.me>
2020-07-28 09:36:07 -0400

    Example of a solution that uses hashtable; however, this doesn't reduce complexity of solution compared to 'brute force' example from previous commit.

--- solutions/two-sum/solution.py
+++ solutions/two-sum/solution.py
@@ -1,11 +1,13 @@
 class Solution:
 
     def factorTarget(self, nums, target):
-        for i, n1 in enumerate(nums, start=0):
-            for j, n2 in enumerate(nums, start=i+1):
-                if j < len(nums):
-                    if n1+nums[j] == target:
-                        return i, j
+        num_val = {}
+        for i, num in enumerate(nums):
+            num_val[num]=i
+        for num in nums:
+            ##Find factors
+            ##Check hashtable for factors
+            ##Return index (numval_[num]) of item using hash table
 
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         factors = self.factorTarget(nums, target)
